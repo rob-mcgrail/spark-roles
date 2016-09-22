6af55a5131d154e8563c5902f3a4c79f9dcffd7b
drjonnicholson <macro@drjonnicholson.com>
2016-09-22 22:46:59 +0100

    Forgot to push class name update

--- install-stubs/database/migrations/create_team_team_role_table.php
+++ install-stubs/database/migrations/create_team_team_role_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRoleTeamTable extends Migration
+class CreateTeamRoleTeamTable extends Migration
 {
 	/**
 	 * Run the migrations.
